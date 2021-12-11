894803977db76c18b1d91f9527f68ad47313891c
cayol1 <cayolegal@hotmail.com>
2021-12-11 14:33:52 -0300

    Se agregó la funcionalidad de cambiar el idioma del texto HTML

---
+++ js/toggleLanguaje.js
@@ -0,0 +1,29 @@
+let link = document.querySelector('#link')
+
+let body = document.querySelector('body')
+
+const toggleLanguage = (event) => {
+
+    event.preventDefault()
+
+    if(window.location.href === "file:///C:/Users/usuario/Documents/CV/index.html" 
+    ) {
+
+        window.location = "en.html"
+    // sessionStorage.setItem("name", "Cayo")
+    // body.innerHTML = sessionStorage.getItem("name")
+
+    } else if(window.location.href === "https://cayolegal1.github.io/Curriculum/") {
+
+        window.location = ""
+    }
+    
+    else {
+        
+        window.location = "index.html"
+    }
+}
+
+
+
+link.addEventListener('click', toggleLanguage)
